1e58a93c721a867a5126a4c673b9cfa4e109f236
danieldivincenzo <danieldivincenzo14@gmail.com>
2025-05-12 21:47:10 +0200

    aggiunto ciclo for

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const esercizio = "FizzBuzz"
+console.log(esercizio)
+
+for (let i = 0; i < 100; i++) {
+    //stampo prima tutti i numeri da 1 a 100
+    let num = i + 1
+
+    // SE un numero è multiplo di 3 e di 5 stampo FizzBuzz
+    // ALTRIMENTI SE è multiplo di 3 stampo Fizz
+    // ALTRIMENTI SE è multiplo di 5 stampo Buzz
+    if ((num % 3) === 0 && (num % 5) === 0) {
+        num = "FizzBuzz"
+    } else if ((num % 3) === 0) {
+        num = "Fizz"
+    } else if ((num % 5) === 0) {
+        num = "Buzz"
+    }
+    console.log(num)
+}
